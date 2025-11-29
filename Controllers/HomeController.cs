68a802683bd2ebec2a7d70125fab0f359b390898
arunpun <83875939+arunpun@users.noreply.github.com>
2025-11-29 17:47:06 +0000

    Implemented app algorithm

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,7 +13,40 @@ namespace HundoMVC.Controllers
 
         public IActionResult App()
         {
-            return View(); 
+            var model = new Hundo
+            {
+                StartValue = 0,
+                EndValue = 100
+            };
+
+            return View(model); 
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult App(Hundo model)
+        {
+            List<string> items = [];
+            List<string> classes = [];
+
+            for (int i = model.StartValue; i <= model.EndValue; i++)
+            {
+                items.Add(i.ToString());
+
+                if (i % 2 == 0)
+                {
+                    classes.Add("bold");
+                }
+                else
+                {
+                    classes.Add("");
+                }
+            }
+
+            model.Results = items;
+            model.Classes = classes;
+
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/Hundo.cs
@@ -0,0 +1,10 @@
+﻿namespace HundoMVC.Models
+{
+    public class Hundo
+    {
+        public int StartValue { get; set; }
+        public int EndValue { get; set; }
+        public List<string> Results { get; set; } = [];
+        public List<string> Classes { get; set; } = [];
+    }
+}
